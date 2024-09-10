411f85395f9d4d232f4a6d945206da18c83b7e87
Zigmunds Rieksts <zigmundsrieksts@inbox.lv>
2024-09-10 15:56:52 +0300

    20-Created different databases for user and general with sqlite

---
+++ src/main/java/com/ecommerce/website/configuration/BaseDatabaseConfiguration.java
@@ -0,0 +1,47 @@
+package com.ecommerce.website.configuration;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.ecommerce.website.dao.base",
+        entityManagerFactoryRef = "baseEntityManager",
+        transactionManagerRef = "baseTransactionManager")
+public class BaseDatabaseConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.base-datasource")
+    DataSource baseDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean baseEntityManager(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("baseDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.ecommerce.website.model.base")
+                .persistenceUnit("base")
+                .build();
+    }
+
+    @Bean
+    PlatformTransactionManager baseTransactionManager(
+            @Qualifier("baseEntityManager") EntityManagerFactory localEntityManagerFactory) {
+        return new JpaTransactionManager(localEntityManagerFactory);
+    }
+}

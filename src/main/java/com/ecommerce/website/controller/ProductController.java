fc9aa3df155d6bf82dac5f9421a3379aa1273378
letoile999 <jribajeva@gmail.com>
2024-09-12 14:37:08 +0300

    3-Product Listing Page

--- src/main/java/com/ecommerce/website/controller/ProductController.java
+++ src/main/java/com/ecommerce/website/controller/ProductController.java
@@ -2,34 +2,48 @@ package com.ecommerce.website.controller;
 
 import com.ecommerce.website.model.base.Product;
 import com.ecommerce.website.service.ProductService;
+import com.ecommerce.website.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
-@RestController
+
+@Controller
 @RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/category/{id}")
-    public ResponseEntity<Page<Product>> getProductsByCategory(
-            @PathVariable Long id,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-
+    public String getProductsByCategory(@PathVariable Long id,
+                                        @RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "10") int size,
+                                        Model model) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Product> productPage = productService.getProductsByCategory(id, pageable);
 
-        return new ResponseEntity<>(productPage, HttpStatus.OK);
+        model.addAttribute("products", productPage.getContent());
+        model.addAttribute("totalPages", productPage.getTotalPages());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("categoryId", id);
+
+        String categoryName = categoryService.getCategoryNameById(id);
+        model.addAttribute("categoryName", categoryName);
+
+        return "ProductListingPage";
     }
 }

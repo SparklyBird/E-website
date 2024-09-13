f8c213ae36215cbde075a1ddaa8903264d078f49
Zigmunds Rieksts <zigmundsrieksts@inbox.lv>
2024-09-13 08:31:13 +0300

    5-admin and user Users in database

--- src/main/resources/schema-user.sql
+++ src/main/resources/schema-user.sql
@@ -1,1 +1,2 @@
-INSERT INTO user (id, login, password, role) VALUES (1, 'admin', 'nimda', 'ADMIN');
+INSERT INTO user (id, login, password, role) VALUES (1, 'admin@gmail.com', '$2a$12$cvH8p7cv/XibVmQcrFf4f.AfFLbty1WuvH5pig90gGwR2w5rkMgKy', 'ADMIN');
+INSERT INTO user (id, login, password, role) VALUES (2, 'user@gmail.com', '$2a$12$cvH8p7cv/XibVmQcrFf4f.AfFLbty1WuvH5pig90gGwR2w5rkMgKy', 'USER');

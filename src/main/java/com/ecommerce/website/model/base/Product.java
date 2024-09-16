d8b3809611b6146bb0ed1a701a70818675a4e723
Zigmunds Rieksts <zigmundsrieksts@inbox.lv>
2024-09-16 10:27:39 +0300

    25. cleaned up minor things and added mysql database code

--- src/main/java/com/ecommerce/website/model/base/Product.java
+++ src/main/java/com/ecommerce/website/model/base/Product.java
@@ -6,11 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
@@ -28,10 +31,13 @@ public class Product {
     private boolean active;
     @Column(name = "units_in_stock")
     private int unitsInStock;
-    @Column(name = "unit_price")
-    private double unitPrice;
+
+    @Column(name = "unit_price", precision = 13, scale = 2)
+    @Min(value = 0, message = "Price must be non-negative")
+    private BigDecimal unitPrice;
 
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
     @OneToMany(mappedBy = "product")
--- src/main/java/com/ecommerce/website/model/base/ProductAttributeValue.java
+++ src/main/java/com/ecommerce/website/model/base/ProductAttributeValue.java
@@ -18,6 +18,7 @@ public class ProductAttributeValue {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 
     @ManyToOne

8601fa0ebc008b208be3f52f4c31efef66c3a8d1
Aman <aman.grewal40@gmail.com>
2024-09-18 20:53:41 +0300

    Wrote some Junit test cases

--- src/main/java/com/ecommerce/website/controller/ErrorController.java
+++ src/main/java/com/ecommerce/website/controller/ErrorController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorController {
 
-    // Mapping for access-denied page
+
     @GetMapping("/access-denied")
     public String accessDenied() {
-        return "access-denied"; // Return the Thymeleaf template for access denied
+        return "access-denied";
     }
 }

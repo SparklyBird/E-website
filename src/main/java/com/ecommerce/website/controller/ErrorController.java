6d0bf25c58cfc49326e0105dfe16810a58315db9
Zigmunds Rieksts <zigmundsrieksts@inbox.lv>
2024-09-20 15:32:40 +0300

    30 - access denied page

--- src/main/java/com/ecommerce/website/controller/ErrorController.java
+++ src/main/java/com/ecommerce/website/controller/ErrorController.java
@@ -1,14 +1,28 @@
 package com.ecommerce.website.controller;
 
+import com.ecommerce.website.dao.base.CategoryRepository;
+import com.ecommerce.website.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorController {
 
+    private final ShoppingCartService shoppingCartService;
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public ErrorController(ShoppingCartService shoppingCartService, CategoryRepository categoryRepository) {
+        this.shoppingCartService = shoppingCartService;
+        this.categoryRepository = categoryRepository;
+    }
 
     @GetMapping("/access-denied")
-    public String accessDenied() {
+    public String accessDenied(Model theModel) {
+        theModel.addAttribute("cartItemCount", shoppingCartService.count());
+        theModel.addAttribute("categories", categoryRepository.findAll());
         return "access-denied";
     }
 }
